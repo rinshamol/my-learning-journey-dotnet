2c5629cc9bad2fa0b697d53ea3e656f6f0a7c5fa
rinshamol <rinshamolks65@gmail.com>
2025-12-04 20:43:54 +0530

    Advanced Routing Techniques

--- BasicApiEndpoints/Program.cs
+++ BasicApiEndpoints/Program.cs
@@ -4,7 +4,37 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello Rinsha!");
 app.MapGet("/hello", () => "Hello Aadhil!");
 app.MapPost("/postMethod", () => "Post Method");
-app.MapPut("/putmethod", () => "Put Method");
-app.MapDelete("/deleteMethod", () => "Delete Method");
+
+
+//Routing parameter
+app.MapGet("/users/{id}/posts/{slug}", (int id, string slug) =>{
+    return $"userId: {id} and posts: {slug}";
+});
+
+//Route constraint
+app.MapGet("/products/{id:int:min(0)}",(int id) => {
+    return $"product ID: {id}";
+});
+
+//optional parameter
+app.MapGet("/report/{year?}", (int year = 2026) =>{
+    return $"Report year: {year}";
+});
+
+//catch-all route using wild-card(*)
+app.MapGet("file/{*filepath}", (string filepath) =>{
+    return $"filepath: {filepath}";
+});
+
+//Query parameters
+app.MapGet("/search", (string? q, int page = 1) =>{
+    return $" Search query: {q}, page: {page}";
+});
+
+app.MapGet("mixture/{id}/{name?}/{*imagepath}", (int id, int data, string imagepath,string name = "Rinsha") =>
+{
+    return $"Mixture contains ID:{id},name: {name}, image path: {imagepath}, data: {data}";  
+});
+
 
 app.Run();
